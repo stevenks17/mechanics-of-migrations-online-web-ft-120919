e68f0f0d38ddba7f40311531229c7ebae385cffe
Steven K <stevenks17@gmail.com>
2020-01-31 15:15:06 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -7,4 +7,9 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   
   def change
   end
+  
+  ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 end
