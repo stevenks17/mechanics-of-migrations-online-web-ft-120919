1164be18e833452400baf929c7c8adfbbb8fa864
Steven K <stevenks17@gmail.com>
2020-01-31 15:22:49 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,32 +1,22 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
-  def up
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+      id INTEGER PRIMARY KEY, 
+      title TEXT, 
+      length INTEGER
+      )
+    SQL
+    ActiveRecord::Base.connection.execute(sql)
   end
- 
-  def down
-  end
-  
+
   def change
     create_table :artists do |t|
       t.string :name
       t.string :genre
       t.integer :age
       t.string :hometown
+    end
   end
-  
-  ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/artists.sqlite"
-)
 
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT,
-  age INTEGER,
-  hometown TEXT
-  )
-SQL
- 
-ActiveRecord::Base.connection.execute(sql)
-end
+end 
